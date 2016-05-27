07659c39d09cffc02469ee8d81caaa19386ad9bb
willriggins <williamriggins2013@gmail.com>
2016-05-27 19:49:24 -0400

    refactor

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -13,6 +13,23 @@ public class Main {
     static String FILE_NAME = "Countries.txt";
 
 
+    //method to read the Countries.txt file
+    public static void readFile(Scanner fileScanner, HashMap<String, ArrayList<Country>> map) {
+        String line = fileScanner.nextLine();
+        String[] columns = line.split("\\|");
+        Country country = new Country(columns[0], columns[1]);
+        String firstLetter = String.valueOf(columns[1].charAt(0));
+        String letter = firstLetter.toUpperCase();
+
+        if (!map.containsKey(letter)) map.put(letter, new ArrayList<>());
+
+        ArrayList<Country> arrayList = map.get(letter);
+        arrayList.add(country);
+    }
+
+
+
+    //method to put each country on its own line in the file i write out for the chosen state
     public static void createList(String finalQuery, HashMap<String, ArrayList<Country>> map) throws IOException {
         File file = new File(finalQuery + "_countries.txt");
         FileWriter fw = new FileWriter(file);
@@ -38,19 +55,12 @@ public class Main {
         Scanner fileScanner = new Scanner(f); // so we can read the countries.txt file
         Scanner scanner = new Scanner(System.in); // so we can read user input
 
+        //while loop that runs readFile method
         while (fileScanner.hasNext()) {
-            String line = fileScanner.nextLine();
-            String[] columns = line.split("\\|");
-            Country country = new Country(columns[0], columns[1]);
-            String firstLetter = String.valueOf(columns[1].charAt(0));
-            String letter = firstLetter.toUpperCase();
-
-            if (!map.containsKey(letter)) map.put(letter, new ArrayList<>());
-
-            ArrayList<Country> arrayList = map.get(letter);
-            arrayList.add(country);
+            readFile(fileScanner, map);
         }
 
+        //ask for user input, convert input to uppercase, and then evaluate for valid input.
         System.out.println("Enter a letter to return all countries whose name starts with that letter.");
         String query = (scanner.nextLine());
         String finalQuery = query.toUpperCase();
